4bbb4a7f6b374ced7dd052ec8cd516c8832bb192
hoboJF <jadenfiero@gmail.com>
2025-01-13 16:53:35 -0500

    made a basic implementation of llama 3.2 vision to make sure everything works properly (it does)

--- imageRecognition/views.py
+++ imageRecognition/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+import ollama
+import os
+from pathlib import Path
 
 def index(request):
+    project_root = Path(__file__).resolve().parent
+    image_path = project_root / 'test.jpg'
+    response = ollama.chat(
+        model = 'llama3.2-vision',
+        messages=[{
+            'role' : 'user',
+            'content' : 'What is in this image?',
+            'images' : [str(image_path)]
+        }]
+    )
+    print(response)
     return render(request, 'imageRecognition/index.html')
